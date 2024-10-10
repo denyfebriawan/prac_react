dc8fc37fb6bd0c9060ef422351e0a0dcf894d20a
denyfebriawan <denyfebriawan@gmail.com>
2024-10-10 12:21:42 +0700

    axios get data

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,47 +1,48 @@
 import { Fragment, useEffect, useRef, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import Button from "../components/Elements/Button/Button";
+import getProducts from "../services/products.service";
 
-const products = [
-  {
-    id: 1,
-    title: "Nike",
-    image:
-      "https://media.istockphoto.com/id/956501428/photo/sport-shoes-on-isolated-white-background.webp?a=1&b=1&s=612x612&w=0&k=20&c=2upDjU33kEx1UwHaB_AdENVWZLHPwVYM-LGb7TLR8W8=",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga unde consectetur distinctio ex dolorem et. Accusamus consectetur vitae sequi reprehenderit dolores odio, magni recusandae voluptatibus asperiores delectus eveniet tempora.",
-    price: 1000000,
-  },
+// const products = [
+//   {
+//     id: 1,
+//     title: "Nike",
+//     image:
+//       "https://media.istockphoto.com/id/956501428/photo/sport-shoes-on-isolated-white-background.webp?a=1&b=1&s=612x612&w=0&k=20&c=2upDjU33kEx1UwHaB_AdENVWZLHPwVYM-LGb7TLR8W8=",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga unde consectetur distinctio ex dolorem et. Accusamus consectetur vitae sequi reprehenderit dolores odio, magni recusandae voluptatibus asperiores delectus eveniet tempora.",
+//     price: 1000000,
+//   },
 
-  {
-    id: 2,
-    title: "Adidas",
-    image:
-      "https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNob2VzfGVufDB8fDB8fHww",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga unde consectetur distinctio ex dolorem et.",
-    price: 3500000,
-  },
+//   {
+//     id: 2,
+//     title: "Adidas",
+//     image:
+//       "https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNob2VzfGVufDB8fDB8fHww",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga unde consectetur distinctio ex dolorem et.",
+//     price: 3500000,
+//   },
 
-  {
-    id: 4,
-    title: "Bata",
-    image:
-      "https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNob2VzfGVufDB8fDB8fHww",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga unde consectetur distinctio ex dolorem et.",
-    price: 3500000,
-  },
+//   {
+//     id: 4,
+//     title: "Bata",
+//     image:
+//       "https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNob2VzfGVufDB8fDB8fHww",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fuga unde consectetur distinctio ex dolorem et.",
+//     price: 3500000,
+//   },
 
-  {
-    id: 3,
-    title: "Jordan",
-    image:
-      "https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNob2VzfGVufDB8fDB8fHww",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    price: 5500000,
-  },
-];
+//   {
+//     id: 3,
+//     title: "Jordan",
+//     image:
+//       "https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNob2VzfGVufDB8fDB8fHww",
+//     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+//     price: 5500000,
+//   },
+// ];
 
 const email = localStorage.getItem("email");
 
@@ -49,6 +50,14 @@ const ProductPage = () => {
   const [cart, setCart] = useState([]);
 
   const [totalPrice, setTotalPrice] = useState(0);
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts((data) => {
+      setProducts(data);
+    });
+  }, []);
 
   useEffect(() => {
     setCart(
@@ -59,7 +68,7 @@ const ProductPage = () => {
   }, []);
 
   useEffect(() => {
-    if (cart.length > 0) {
+    if (products.length > 0 && cart.length > 0) {
       const sum = cart.reduce((acc, item) => {
         const product = products.find((product) => product.id === item.id);
         return acc + product.price * item.qty;
@@ -67,7 +76,7 @@ const ProductPage = () => {
       setTotalPrice(sum);
       localStorage.setItem("cart", JSON.stringify(cart));
     }
-  }, [cart]);
+  }, [cart, products]);
 
   const handleLogout = () => {
     localStorage.removeItem("email");
@@ -107,19 +116,20 @@ const ProductPage = () => {
       </div>
       <div className="flex justify-center p-3">
         <div className="w-3/5 flex flex-wrap">
-          {products.map((product) => (
-            <CardProduct key={product.id}>
-              <CardProduct.Header urlImage={product.image} />
-              <CardProduct.Body title={product.title}>
-                {product.description}
-              </CardProduct.Body>
-              <CardProduct.Footer
-                price={product.price}
-                id={product.id}
-                handleAddToCart={handleAddToCart}
-              />
-            </CardProduct>
-          ))}
+          {products.length > 0 &&
+            products.map((product) => (
+              <CardProduct key={product.id}>
+                <CardProduct.Header urlImage={product.image} />
+                <CardProduct.Body title={product.title}>
+                  {product.description}
+                </CardProduct.Body>
+                <CardProduct.Footer
+                  price={product.price}
+                  id={product.id}
+                  handleAddToCart={handleAddToCart}
+                />
+              </CardProduct>
+            ))}
         </div>
         <div className="w-2/5">
           <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Cart</h1>
@@ -133,38 +143,39 @@ const ProductPage = () => {
               </tr>
             </thead>
             <tbody>
-              {cart.map((item) => {
-                const product = products.find(
-                  (product) => product.id === item.id
-                );
-                return (
-                  <tr key={item.id}>
-                    <td>{product.title}</td>
-                    <td>
-                      Rp
-                      {product.price.toLocaleString("id-ID", {
-                        styles: "currency",
-                        currency: "IDR",
-                      })}
-                    </td>
-                    <td>{item.qty}</td>
-                    <td>
-                      Rp
-                      {(product.price * item.qty).toLocaleString("id-ID", {
-                        styles: "currency",
-                        currency: "IDR",
-                      })}
-                    </td>
-                  </tr>
-                );
-              })}
+              {products.length > 0 &&
+                cart.map((item) => {
+                  const product = products.find(
+                    (product) => product.id === item.id
+                  );
+                  return (
+                    <tr key={item.id}>
+                      <td>{product.title.substring(0,10)}...</td>
+                      <td>
+                        $
+                        {product.price.toLocaleString("id-ID", {
+                          styles: "currency",
+                          currency: "USD",
+                        })}
+                      </td>
+                      <td>{item.qty}</td>
+                      <td>
+                        $
+                        {(product.price * item.qty).toLocaleString("id-ID", {
+                          styles: "currency",
+                          currency: "USD",
+                        })}
+                      </td>
+                    </tr>
+                  );
+                })}
               <tr ref={totalPriceRef}>
                 <td colSpan={3}>Total Price</td>
                 <td>
-                  Rp
+                  $
                   {totalPrice.toLocaleString("id-ID", {
                     styles: "currency",
-                    currency: "IDR",
+                    currency: "USD",
                   })}
                 </td>
               </tr>
--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -14,7 +14,11 @@ const Header = (props) => {
   const { urlImage } = props;
   return (
     <a href="#">
-      <img className="p-8 rounded-t-lg" src={urlImage} alt="product" />
+      <img
+        className="p-8 rounded-t-lg h-60 w-full"
+        src={urlImage}
+        alt="product"
+      />
     </a>
   );
 };
@@ -25,9 +29,11 @@ const Body = (props) => {
     <div className="px-5 pb-5 h-full">
       <a href="">
         <h5 className="text-xl font-semibold tracking-tight text-white">
-          {title}
+          {title.substring(0, 20)}...
         </h5>
-        <p className="text-m text-white text-justify">{children}</p>
+        <p className="text-m text-white text-justify">
+          {children.substring(0, 50)}...
+        </p>
       </a>
     </div>
   );
@@ -38,8 +44,8 @@ const Footer = (props) => {
   return (
     <div className="flex items-center justify-between p-3">
       <span className="text-3xl font-bold text-white">
-        Rp
-        {price.toLocaleString("id-ID", { styles: "currency", currency: "IDR" })}
+        $
+        {price.toLocaleString("id-ID", { styles: "currency", currency: "USD" })}
       </span>
       <Button variant="bg-blue-700" onClick={() => handleAddToCart(id)}>
         Add To Cart
